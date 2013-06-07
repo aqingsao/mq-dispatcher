be8be7f35d05cd1cc19406e1962c20d9fbfe5fba
aqingsao <xqzhang@thoughtworks.com>
2013-06-07 16:30:07 +0800

    use a map to keep the connections between deviceId and server

--- src/main/java/com/thoughtworks/i1/mq/service/DispatcherService.java
+++ src/main/java/com/thoughtworks/i1/mq/service/DispatcherService.java
@@ -1,19 +1,56 @@
 package com.thoughtworks.i1.mq.service;
 
+import com.google.common.base.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jms.Message;
+import java.util.*;
 
 public class DispatcherService {
     private static final Logger LOGGER = LoggerFactory.getLogger(DispatcherService.class);
+    private Map<String, List<String>> server2DeviceIds;
 
-    public void onConnected(String uri, String deviceId) {
-        LOGGER.info("Device {1} connected to server {2}", deviceId, uri);
+    public DispatcherService() {
+        server2DeviceIds = new HashMap();
+    }
 
+    public void onConnected(String server, String deviceId) {
+        LOGGER.info("Device {1} connected to server {2}", deviceId, server);
+        for (Map.Entry<String, List<String>> entry : server2DeviceIds.entrySet()) {
+            List<String> deviceIds = entry.getValue();
+            if (deviceIds.contains(deviceId)) {
+                deviceIds.remove(deviceId);
+                LOGGER.info("Will remove device {1} from server {2}", deviceId, server);
+            }
+        }
+        List<String> deviceIds = server2DeviceIds.get(server);
+        if (deviceIds == null) {
+            deviceIds = new ArrayList<String>();
+            server2DeviceIds.put(server, deviceIds);
+        }
+        deviceIds.add(deviceId);
+        LOGGER.info("Add device {1} from server {2}", deviceId, server);
+    }
+
+    public List<String> getDeviceIds(String server) {
+        if (!server2DeviceIds.containsKey(server)) {
+            return Collections.EMPTY_LIST;
+        }
+        return server2DeviceIds.get(server);
     }
 
     public void onMessage(Message message) {
         LOGGER.info("Receive message {1}", message);
+    }
+
+    public Optional<String> getServer(String deviceId) {
+        for (Map.Entry<String, List<String>> entry : server2DeviceIds.entrySet()) {
+            List<String> deviceIds = entry.getValue();
+            if (deviceIds.contains(deviceId)) {
+                return Optional.of(entry.getKey());
+            }
+        }
+        return Optional.absent();
     }
 }
--- src/test/java/com/thoughtworks/i1/mq/service/DispatcherServiceTest.java
+++ src/test/java/com/thoughtworks/i1/mq/service/DispatcherServiceTest.java
@@ -1,11 +1,66 @@
 package com.thoughtworks.i1.mq.service;
 
+import com.google.common.base.Optional;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class DispatcherServiceTest {
 
-    @Test
-    public void testOnConnected() throws Exception {
+    private DispatcherService dispatcherService;
 
+    @Before
+    public void before() {
+        dispatcherService = new DispatcherService();
+    }
+
+    @Test
+    public void getDeviceIds_should_return_empty_when_no_deviceIds_connect_to_a_server() throws Exception {
+        List<String> deviceIds = dispatcherService.getDeviceIds("1");
+        assertThat(deviceIds.isEmpty(), is(true));
+    }
+
+    @Test
+    public void getDeviceIds_should_return_deviceIds_when_at_least_1_deviceIds_connect_to_a_server() throws Exception {
+        dispatcherService.onConnected("1", "11");
+        List<String> deviceIds = dispatcherService.getDeviceIds("1");
+        assertThat(deviceIds.size(), is(1));
+        assertThat(deviceIds.get(0), is("11"));
+    }
+
+    @Test
+    public void getServer_should_return_absent_when_device_11_does_not_connect_to_a_server() throws Exception {
+        Optional<String> actual = dispatcherService.getServer("11");
+        assertThat(actual.isPresent(), is(false));
+    }
+
+    @Test
+    public void getServer_should_return_1_when_device_11_connected_to_server_1() throws Exception {
+        dispatcherService.onConnected("1", "11");
+        Optional<String> actual = dispatcherService.getServer("11");
+        assertThat(actual.isPresent(), is(true));
+        assertThat(actual.get(), is("1"));
+    }
+
+    @Test
+    public void onConnected_should_remove_from_previous_server_when_device_connected_to_new_server() {
+        dispatcherService.onConnected("1", "11");
+        dispatcherService.onConnected("2", "11");
+        assertThat(dispatcherService.getDeviceIds("1").isEmpty(), is(true));
+        assertThat(dispatcherService.getDeviceIds("2").size(), is(1));
+        assertThat(dispatcherService.getDeviceIds("2").get(0), is("11"));
+    }
+
+    @Test
+    public void onConnected_should_add_more_than_1_deviceIds_to_a_server() {
+        dispatcherService.onConnected("1", "11");
+        dispatcherService.onConnected("1", "12");
+        assertThat(dispatcherService.getDeviceIds("1").size(), is(2));
+        assertThat(dispatcherService.getDeviceIds("1").get(0), is("11"));
+        assertThat(dispatcherService.getDeviceIds("1").get(1), is("12"));
     }
 }
